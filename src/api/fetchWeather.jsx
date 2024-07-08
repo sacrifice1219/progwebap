d79dd607bde6cf412ded342171f3b17dd5f6b6e4
lucky favour <luckyfavour1219@gmail.com>
2024-07-08 16:18:00 -0700

    commit

--- src/api/fetchWeather.jsx
+++ src/api/fetchWeather.jsx
@@ -1,7 +1,13 @@
 import axios from "axios";
+// App.js
+// import dotenv from 'dotenv';
+// dotenv.config();
+
+// const apiKey = process.env.REACT_APP_API_KEY;
+
 
 const URL = 'https:api.openweathermap.org/data/2.5/weather';
-const API_KEY = '0daec2178920b3eb3e8815512c28096c';
+const API_KEY ='0daec2178920b3eb3e8815512c28096c';
 
 export const fetchWeather = async (query) => {
     const {data} = await axios.get(URL, {
